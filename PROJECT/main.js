af60d81b14a725a3b7b0c5ff8da3bb672e44e3fa
Maria Malysa <mariastud2023@gmail.com>
2025-03-21 21:20:57 +0200

    wtf

--- PROJECT/main.js
+++ PROJECT/main.js
@@ -1,19 +1,17 @@
-// Obsługa wysłania formularza
-contactForm.addEventListener('submit', (e) => {
-    e.preventDefault(); // Zapobiega wysłaniu formularza
 
-    const country = countrySelect.value;
-    const hotel = hotelSelect.value;
-    const message = document.getElementById('message').value;
+    // Obsługa wysłania formularza
+    contactForm.addEventListener('submit', (e) => {
+        e.preventDefault(); 
 
-    if (!country || !hotel || !message) {
-        alert('Please fill out all fields.');
-        return;
-    }
+        const country = countrySelect.value;
+        const hotel = hotelSelect.value;
+        const message = document.getElementById('message').value;
 
-    // Resetowanie formularza
-    contactForm.reset();
-
-    // Wyświetl potwierdzenie (bez wysyłania na serwer)
-    alert(`Message sent!\nCountry: ${country}\nHotel: ${hotel}\nMessage: ${message}`);
+        if (!country || !hotel || !message) {
+            alert('Please fill out all fields.');
+            return;
+        }
+        alert(`Message sent!\nCountry: ${country}\nHotel: ${hotel}\nMessage: ${message}`);
+        contactForm.reset(); // Resetuje formularz
+    });
 });
